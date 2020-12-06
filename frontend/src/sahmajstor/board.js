68cb0768fe3e19087e4843534f3841886e338784
Stefan Petrovic <stef.ptr@protonmail.com>
2020-12-06 22:43:00 +0100

    All figures are placed in their respective places.

--- frontend/src/sahmajstor/board.js
+++ frontend/src/sahmajstor/board.js
@@ -1,13 +1,48 @@
 import P5 from 'p5'
+import { figureImagePath } from './figures.js'
 
 
 const sketch = s => {
 
-  function Square(x, y, fill) {
-    // One square, which can hold one of the available figures
+  let squareSize = 70
+  let ROWS = 8
+  let COLS = 8
 
-    // Size of square
-    let squareSize = 60
+  function Figure(type, player) {
+    // Load the associated image for this figure
+    if (player === 'white') {
+      if (type === 'pawn') {
+        this.image = s.loadImage(figureImagePath.pawnWhiteImagePath)
+      } else if (type === 'knight') {
+        this.image = s.loadImage(figureImagePath.knightWhiteImagePath)
+      } else if (type === 'bishop') {
+        this.image = s.loadImage(figureImagePath.bishopWhiteImagePath)
+      } else if (type === 'rook') {
+        this.image = s.loadImage(figureImagePath.rookWhiteImagePath)
+      } else if (type === 'queen') {
+        this.image = s.loadImage(figureImagePath.queenWhiteImagePath)
+      } else if (type === 'king') {
+        this.image = s.loadImage(figureImagePath.kingWhiteImagePath)
+      }
+    } else if (player === 'black') {
+      if (type === 'pawn') {
+        this.image = s.loadImage(figureImagePath.pawnBlackImagePath)
+      } else if (type === 'knight') {
+        this.image = s.loadImage(figureImagePath.knightBlackImagePath)
+      } else if (type === 'bishop') {
+        this.image = s.loadImage(figureImagePath.bishopBlackImagePath)
+      } else if (type === 'rook') {
+        this.image = s.loadImage(figureImagePath.rookBlackImagePath)
+      } else if (type === 'queen') {
+        this.image = s.loadImage(figureImagePath.queenBlackImagePath)
+      } else if (type === 'king') {
+        this.image = s.loadImage(figureImagePath.kingBlackImagePath)
+      }
+    }
+  }
+
+  function Square(x, y, fill, figure=null) {
+    // One square, which can hold one of the available figures
 
     // Coordinates of the square
     this.x = x*squareSize
@@ -22,16 +57,27 @@ const sketch = s => {
     this.inactiveFill = fill
     this.activeFill = s.color(255, 204, 0)
 
+    // Figure associated with this square
+    this.figure = figure
+    console.log(figure)
+
     // Used to update display of this particular square
     this.display = function() {
       s.fill(this.fill)
       s.square(this.x, this.y, squareSize)
+      if (this.figure) {
+        s.image(this.figure.image, this.x, this.y, squareSize, squareSize)
+      }
     }
 
     // Returns which squares the figure on this particular square is allowed to
     // move to
     this.getMoves = function() {
-
+      if (this.figure) {
+        return true
+      } else {
+        return null
+      }
     }
 
     // Returns whether this particular square was clicked or not
@@ -57,32 +103,68 @@ const sketch = s => {
 
   let squares = []
 
-  let img;
-  s.preload = () => {
-    img = s.loadImage('pawn_white.png');
-  }
-
   s.setup = () => {
-    s.createCanvas(600, 600)
+    s.createCanvas(squareSize*ROWS, squareSize*COLS)
 
     // Draw chess board
     let currentIsWhite = true
     let fill = 225
-    for (let row = 0; row < 10; row += 1) {
-      for (let col = 0; col < 10; col += 1) {
+    let figure
+    for (let row = 0; row < ROWS; row += 1) {
+      for (let col = 0; col < COLS; col += 1) {
         if (currentIsWhite) {
-          fill = 255
+          fill = s.color(181, 136, 99)
         } else {
-          fill = 0
+          fill = s.color(240, 217, 181)
         }
-        let square = new Square(row, col, fill)
+
+        // Figures
+        if (row === 0 && col === 0) {
+          figure = new Figure('rook', 'black')
+        } else if (row === 0 && col === 1) {
+          figure = new Figure('knight', 'black')
+        } else if (row === 0 && col === 2) {
+          figure = new Figure('bishop', 'black')
+        } else if (row === 0 && col === 3) {
+          figure = new Figure('queen', 'black')
+        } else if (row === 0 && col === 4) {
+          figure = new Figure('king', 'black')
+        } else if (row === 0 && col === 5) {
+          figure = new Figure('bishop', 'black')
+        } else if (row === 0 && col === 6) {
+          figure = new Figure('knight', 'black')
+        } else if (row === 0 && col === 7) {
+          figure = new Figure('rook', 'black')
+        } else if (row === 1) {
+          figure = new Figure('pawn', 'black')
+        } else if (row === 7 && col === 0) {
+          figure = new Figure('rook', 'white')
+        } else if (row === 7 && col === 1) {
+          figure = new Figure('knight', 'white')
+        } else if (row === 7 && col === 2) {
+          figure = new Figure('bishop', 'white')
+        } else if (row === 7 && col === 3) {
+          figure = new Figure('queen', 'white')
+        } else if (row === 7 && col === 4) {
+          figure = new Figure('king', 'white')
+        } else if (row === 7 && col === 5) {
+          figure = new Figure('bishop', 'white')
+        } else if (row === 7 && col === 6) {
+          figure = new Figure('knight', 'white')
+        } else if (row === 7 && col === 7) {
+          figure = new Figure('rook', 'white')
+        } else if (row === 6) {
+          figure = new Figure('pawn', 'white')
+        } else {
+          figure = null
+        }
+        let square = new Square(col, row, fill, figure)
         square.display()
         squares.push(square)
         currentIsWhite = !currentIsWhite
       }
       currentIsWhite = !currentIsWhite
     }
-    s.image(img, 0, 0);
   }
 
   s.mousePressed = () => {
---
+++ frontend/src/sahmajstor/figures.js
@@ -0,0 +1,15 @@
+export let figureImagePath = {
+  pawnWhiteImagePath: 'figures/white/pawn.png',
+  knightWhiteImagePath: 'figures/white/knight.png',
+  bishopWhiteImagePath: 'figures/white/bishop.png',
+  rookWhiteImagePath: 'figures/white/rook.png',
+  queenWhiteImagePath: 'figures/white/queen.png',
+  kingWhiteImagePath: 'figures/white/king.png',
+
+  pawnBlackImagePath: 'figures/black/pawn.png',
+  knightBlackImagePath: 'figures/black/knight.png',
+  bishopBlackImagePath: 'figures/black/bishop.png',
+  rookBlackImagePath: 'figures/black/rook.png',
+  queenBlackImagePath: 'figures/black/queen.png',
+  kingBlackImagePath: 'figures/black/king.png'
+}
