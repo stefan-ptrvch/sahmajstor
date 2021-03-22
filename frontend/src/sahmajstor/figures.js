c74907b373fd7e1640d65a9d4a1f5fe022d4c8e2
Stefan Petrovic <stefan@idiotech.rs>
2021-03-22 21:35:56 +0100

    Added collision for rook.

--- frontend/src/sahmajstor/figures.js
+++ frontend/src/sahmajstor/figures.js
@@ -219,36 +219,76 @@ export function bishopMoves(x, y, player, board) {
   return moves
 }
 
-export function rookMoves(x, y, player) {
-  console.log(player)
+export function rookMoves(x, y, player, board) {
+  // The rook has four possible move types
   let newX, newY
   let moves = []
 
-  newX = x
+  newX = x + 1
   newY = y
   while (newX >= 0 && newX < COLS && newY >= 0 && newY < ROWS) {
-    moves.push({x: newX, y: newY})
+    // If there's a figure in this square, and if it's the opponent's figure,
+    // we add this move to the moves list, else we just break
+    if (!board[newY][newX].figure) {
+      moves.push({x: newX, y: newY})
+    } else if (board[newY][newX].figure.player !== player) {
+      moves.push({x: newX, y: newY})
+      break
+    } else {
+      break
+    }
+
     newX += 1
   }
 
-  newX = x
+  newX = x - 1
   newY = y
   while (newX >= 0 && newX < COLS && newY >= 0 && newY < ROWS) {
-    moves.push({x: newX, y: newY})
+    // If there's a figure in this square, and if it's the opponent's figure,
+    // we add this move to the moves list, else we just break
+    if (!board[newY][newX].figure) {
+      moves.push({x: newX, y: newY})
+    } else if (board[newY][newX].figure.player !== player) {
+      moves.push({x: newX, y: newY})
+      break
+    } else {
+      break
+    }
+
     newX -= 1
   }
 
   newX = x
-  newY = y
+  newY = y + 1
   while (newX >= 0 && newX < COLS && newY >= 0 && newY < ROWS) {
-    moves.push({x: newX, y: newY})
+    // If there's a figure in this square, and if it's the opponent's figure,
+    // we add this move to the moves list, else we just break
+    if (!board[newY][newX].figure) {
+      moves.push({x: newX, y: newY})
+    } else if (board[newY][newX].figure.player !== player) {
+      moves.push({x: newX, y: newY})
+      break
+    } else {
+      break
+    }
+
     newY += 1
   }
 
   newX = x
-  newY = y
+  newY = y - 1
   while (newX >= 0 && newX < COLS && newY >= 0 && newY < ROWS) {
-    moves.push({x: newX, y: newY})
+    // If there's a figure in this square, and if it's the opponent's figure,
+    // we add this move to the moves list, else we just break
+    if (!board[newY][newX].figure) {
+      moves.push({x: newX, y: newY})
+    } else if (board[newY][newX].figure.player !== player) {
+      moves.push({x: newX, y: newY})
+      break
+    } else {
+      break
+    }
+
     newY -= 1
   }
 
