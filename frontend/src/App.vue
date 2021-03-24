7bd4dd85244fbea9e57403a7587cd4ca12632356
Stefan Petrovic <stefan@idiotech.rs>
2021-03-24 21:10:15 +0100

    Added Vuetify to project.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,19 @@
 <template>
-  <router-view/>
+  <v-app>
+    <v-main>
+      <home />
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+import Home from './views/Home'
+
+export default {
+  name: 'App',
+
+  components: {
+    Home,
+  }
+}
+</script>
